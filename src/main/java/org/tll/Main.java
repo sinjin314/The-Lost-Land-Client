69376e520b5e1fe0501cbe6a57a767a9aecea234
Abdelhadi Hasnaoui <abdelhadi.hasnaoui@protonmail.com>
2024-03-01 23:06:32 +0100

    lwjgl init
    initialized the lwjgl dependencies, and add a test class

--- src/main/java/org/tll/Main.java
+++ src/main/java/org/tll/Main.java
@@ -1,5 +1,6 @@
 package org.tll;
 
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
---
+++ src/main/java/test/Launcher.java
@@ -0,0 +1,10 @@
+package test;
+
+import org.lwjgl.Version;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        System.out.println("This is the lwjgl version!" + Version.getVersion() + "!");
+    }
+}

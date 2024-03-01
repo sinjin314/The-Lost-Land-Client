8ee40f1961ae352a8addc407800e8ea243cac625
Abdelhadi Hasnaoui <abdelhadi.hasnaoui@protonmail.com>
2024-03-02 00:30:32 +0100

    OpenGL basics part two
    only forgotten to push some other file

--- src/main/java/org/tll/core/WindowManager.java
+++ src/main/java/org/tll/core/WindowManager.java
@@ -1,4 +1,161 @@
 package org.tll.core;
 
+import org.joml.Matrix4f;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL33;
+import org.lwjgl.system.MemoryUtil;
+
 public class WindowManager {
+
+    public static final float FOV = (float) Math.toRadians(60.0f);
+    public static final float Z_NEAR = 0.01f;
+    public static final float Z_FAR = 1000.0f;
+
+    private final String title;
+
+    private int width, height;
+    private long window;
+
+    private boolean resize, VSync;
+
+    private final Matrix4f projectionMatrix;
+
+    public WindowManager(String title, int width, int height, boolean VSync) {
+        this.title = title;
+        this.width = width;
+        this.height = height;
+        this.VSync = VSync;
+        this.projectionMatrix = new Matrix4f();
+    }
+
+    public void init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if(!GLFW.glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        GLFW.glfwDefaultWindowHints();
+        GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GL33.GL_FALSE);
+        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GL33.GL_TRUE);
+        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 3);
+        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 3);
+        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
+        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GL33.GL_TRUE);
+
+        boolean maximised = false;
+        if(width == 0 || height == 0) {
+            maximised = true;
+            width = 100;
+            height = 100;
+            GLFW.glfwWindowHint(GLFW.GLFW_MAXIMIZED, GL33.GL_TRUE);
+            maximised = true;
+        }
+
+        window = GLFW.glfwCreateWindow(width, height, title, MemoryUtil.NULL, MemoryUtil.NULL);
+        if(window == MemoryUtil.NULL) {
+            throw new RuntimeException("Failed to create the GLFW window");
+        }
+
+        GLFW.glfwSetFramebufferSizeCallback(window, (window, width, height) -> {
+            this.width = width;
+            this.height = height;
+            this.setResized(true);
+        });
+
+        GLFW.glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+            if(key == GLFW.GLFW_KEY_ESCAPE && action == GLFW.GLFW_RELEASE) {
+                GLFW.glfwSetWindowShouldClose(window, true);
+            }
+        });
+
+        if(maximised) {
+            GLFW.glfwMaximizeWindow(window);
+        } else {
+            GLFWVidMode vidMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+            GLFW.glfwSetWindowPos(window, (vidMode.width() - width) / 2, (vidMode.height() - height) / 2);
+
+        }
+
+        GLFW.glfwMakeContextCurrent(window);
+
+        GL.createCapabilities();
+
+        GL33.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        GL33.glEnable(GL33.GL_DEPTH_TEST);
+        GL33.glEnable(GL33.GL_STENCIL_TEST);
+        GL33.glEnable(GL33.GL_CULL_FACE);
+        GL33.glCullFace(GL33.GL_BACK);
+    }
+
+    public void update() {
+        GLFW.glfwSwapBuffers(window);
+        GLFW.glfwPollEvents();
+    }
+
+    public void cleanUp() {
+        GLFW.glfwDestroyWindow(window);
+    }
+
+    public void setClearColor(float r, float g, float b, float alpha) {
+        GL33.glClearColor(r, g, b, alpha);
+    }
+
+    public boolean isKeyPressed(int keyCode) {
+        return GLFW.glfwGetKey(window, keyCode) == GLFW.GLFW_PRESS;
+    }
+
+    public boolean shouldClose() {
+        return GLFW.glfwWindowShouldClose(window);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public  void setTitle(String title) {
+        GLFW.glfwSetWindowTitle(window, title);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public long getWindow() {
+        return window;
+    }
+
+    public boolean isResized() {
+        return resize;
+    }
+
+    public void setResized(boolean resized) {
+        this.resize = resized;
+    }
+
+    public boolean isVSync() {
+        return VSync;
+    }
+
+    public Matrix4f getProjectionMatrix() {
+        return projectionMatrix;
+    }
+
+    public Matrix4f updateProjectionMatrix() {
+        float aspectRatio = (float) width / height;
+        return projectionMatrix.setPerspective(FOV, aspectRatio, Z_NEAR, Z_FAR);
+    }
+
+    public Matrix4f updateProjectionMatrix(Matrix4f matrix, int width, int height) {
+
+        float aspectRatio = (float) width / height;
+        return projectionMatrix.setPerspective(FOV, aspectRatio, Z_NEAR, Z_FAR);
+    }
 }
--- src/main/java/test/Launcher.java
+++ src/main/java/test/Launcher.java
@@ -1,10 +1,19 @@
 package test;
 
 import org.lwjgl.Version;
+import org.tll.core.WindowManager;
 
 public class Launcher {
 
     public static void main(String[] args) {
         System.out.println("This is the lwjgl version!" + Version.getVersion() + "!");
+        WindowManager window = new WindowManager("Test", 800, 600, true);
+        window.init();
+
+        while (!window.shouldClose()) {
+            window.update();
+        }
+
+        window.cleanUp();
     }
 }
